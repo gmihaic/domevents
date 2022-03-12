9d74b2fe8224ec2e3bc7d84a81bb9baf1373bb00
gmihaic <100798898+gmihaic@users.noreply.github.com>
2022-03-12 12:10:25 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,37 +1,56 @@
+var listElement = {	
+	
+	"handleClick": function(event) {
+		event.target.classList.toggle("done");
+	},
+
+	"addClickEvent": function(li) {
+		li.addEventListener("click", listElement.handleClick);	
+	},
+
+	"addDeleteButton": function(li) {
+		var btn = document.createElement("button");
+		btn.appendChild(document.createTextNode("Delete me!"));	
+		li.append(btn);
+
+		btn.addEventListener("click", function(){
+			listElement.remove(li);
+		});
+	},
+
+	"remove": function(li) {
+		li.remove();
+	}
+
+};
+
 var button = document.getElementById("enter");
 var input = document.getElementById("userinput");
 var ul = document.querySelector("ul");
 
-function inputLength() {
-	return input.value.length;
-}
-
 /* list click events */
-function listClickHandle(event)
-{	
+//old
+/*function handleListElementClick(event) {		
 	event.target.classList.toggle("done");
 }
 
-function listElementAddListenClickEvent(li)
-{
-	li.addEventListener("click", listClickHandle);	
+function addClickEventToListElement(li) {
+	li.addEventListener("click", handleListElementClick);	
 }
 
-function listElementAddDeleteButton(li)
-{
+function addDeleteButtonToListElement(li) {
 	var btn = document.createElement("button");
 	btn.appendChild(document.createTextNode("Delete me!"));	
 	li.append(btn);
 
 	btn.addEventListener("click", function(){
-		deleteListFromButton(li);
+		deleteListElement(li);
 	});
 }
 
-function deleteListFromButton(li)
-{
+function deleteListElement(li) {
 	li.remove();
-}
+}*/
 /* end list click events */
 
 function createListElement() {
@@ -40,18 +59,18 @@ function createListElement() {
 	ul.appendChild(li);
 	input.value = "";
 
-	listElementAddListenClickEvent(li);
-	listElementAddDeleteButton(li);
+	listElement.addClickEvent(li); //addClickEventToListElement(li);
+	listElement.addDeleteButton(li); //addDeleteButtonToListElement(li);
 }
 
 function addListAfterClick() {
-	if (inputLength() > 0) {
+	if (input.value.length > 0) {
 		createListElement();
 	}
 }
 
 function addListAfterKeypress(event) {
-	if (inputLength() > 0 && event.keyCode === 13) {
+	if (input.value.length > 0 && event.keyCode === 13) {
 		createListElement();
 	}
 }
@@ -62,7 +81,7 @@ input.addEventListener("keypress", addListAfterKeypress);
 
 var lists_existing = document.querySelectorAll("ul li");
 
-lists_existing.forEach(function(item, idx){
-	listElementAddListenClickEvent(item);
-	listElementAddDeleteButton(item);
+lists_existing.forEach(function(item){
+	listElement.addClickEvent(item); //addClickEventToListElement(item);
+	listElement.addDeleteButton(item); //addDeleteButtonToListElement(item);
 });
